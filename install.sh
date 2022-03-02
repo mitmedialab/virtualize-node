5bc4a8bdc6204d67750c28b25eea5dda2e168722
Jon Ferguson <jon@jef.us>
2022-03-02 10:26:13 -0500

    donebugging

--- install.sh
+++ install.sh
@@ -12,29 +12,18 @@ VIRTUALIZE_NODE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && 
 source "$VIRTUALIZE_NODE_DIR/version.bash"
 node_version=$VERSION
 
-echo 1
 git clone https://github.com/tj/n $VIRTUALIZE_NODE_DIR/n
 (
-echo 2
     cd $VIRTUALIZE_NODE_DIR/n
-echo 3
     export PREFIX=$VIRTUALIZE_NODE_DIR
-echo 4
     make
-echo 5
     make install
-echo 6
 )
-echo 7
 #rm -rf $VIRTUALIZE_NODE_DIR/n
-echo 8
 N_PREFIX=$VIRTUALIZE_NODE_DIR $VIRTUALIZE_NODE_DIR/bin/n $node_version
-echo 9
 
 $VIRTUALIZE_NODE_DIR/bin/npm install -g yarn
-echo 10
 
 echo "node installed"
-echo 11
 
 exit

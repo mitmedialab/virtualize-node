4f0d02278e2184cdaa21ea0e30d106a5b8859d66
Jon Ferguson <jon@jef.us>
2023-05-20 10:58:07 -0400

    fix bash bug, MANPATH, and version setting

--- setup.sh
+++ setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+DEFAULT_NODE_VERSION="16.13.2"
+
 if [[ ! $BASH_SOURCE ]]; then
     echo "You cannot source this script. Run it as ./$0" >&2
     exit 33
 fi
 
 VIRTUALIZE_NODE_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE}" )" &> /dev/null && pwd )
+
+if [[ $1 ]]; then
+    VIRTUALIZE_NODE_VERSION=$1
+elif [[ ! $VIRTUALIZE_NODE_VERSION ]]; then
+    VIRTUALIZE_NODE_VERSION=$DEFAULT_NODE_VERSION
+fi
 
 #node_version=14.16
 # Retrieve node version to be installed/activated
@@ -25,16 +33,10 @@ git clone https://github.com/tj/n $VIRTUALIZE_NODE_DIR/n
     rm -rf $VIRTUALIZE_NODE_DIR/n
 )
 
-N_PREFIX=$VIRTUALIZE_NODE_DIR/node $VIRTUALIZE_NODE_DIR/node/bin/n $INSTALL_VERSION
+N_PREFIX=$VIRTUALIZE_NODE_DIR/node $VIRTUALIZE_NODE_DIR/node/bin/n $VIRTUALIZE_NODE_VERSION
 export PATH="$VIRTUALIZE_NODE_DIR/node/bin:$PATH"  # apparently npm can't find it's own node install?
 $VIRTUALIZE_NODE_DIR/node/bin/npm install -g yarn
 
 echo "node installed"
 
 exit
-
-
-## Notes:
-## rename activate.bash -> activate?
-## rename repo to virtualize-node?
-## centralize versions
